ef2d9fd62e3895f9e58f79ea38f04161c13cdd9e
emmanuel <eajejohnson@gmail.com>
2025-05-22 13:41:06 +0100

    fixed image upload

--- models/Message.js
+++ models/Message.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
+  text: {
+    type: String,
+    required: function () {
+      return !this.attachment;
+    }
+  },
+  attachment: { type: String }, // add this if not already in the schema
   timestamp: { type: Date, default: Date.now }
 }, { timestamps: true });
 
